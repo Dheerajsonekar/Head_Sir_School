711b4625ac75d15a5740fe2c25cf8fbfe0f5bee5
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-07-30 12:50:51 +0530

    up8

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -6,4 +6,17 @@ declare var process: {
     MONGO_URI?: string;
     JWT_SECRET?: string;
   };
+  exit(code?: number): never;
+};
+
+declare var console: {
+  log(...data: any[]): void;
+  error(...data: any[]): void;
+  warn(...data: any[]): void;
+  info(...data: any[]): void;
+};
+
+declare var Buffer: {
+  from(data: string | number[] | ArrayBuffer, encoding?: string): any;
+  isBuffer(obj: any): boolean;
 };
